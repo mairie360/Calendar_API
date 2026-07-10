d6d05a08f722a84f8d77e79ce16b1da605c424af
Quentin Tennerel <tennerelquentin@gmail.com>
2026-07-10 18:34:23 +0200

    fix: fix tests

--- tests/queries/event/create.rs
+++ tests/queries/event/create.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use crate::common::get_pool; // Utilisation de ta fonction utilitaire existante
 use calendar_api::database::event::create::{
     query::{create_event_by_group_query, create_event_by_user_query},
@@ -7,17 +9,21 @@ use calendar_api::database::event::create::{
 };
 use chrono::Utc;
 use mairie360_api_lib::test_setup::queries_setup::get_shared_db;
+use tokio::time::sleep;
 
 #[sqlx::test]
 async fn test_create_event_by_user_success() {
     let (_container, host) = get_shared_db().await;
     let pool = get_pool(host.to_string()).await;
 
+    let start_date = Utc::now();
+    let _ = sleep(Duration::from_secs(1));
+    let end_date = start_date + chrono::Duration::days(10);
     let view = CreateEventByUserQueryView::new(
         "Test Event",
         Some("Description"),
-        Utc::now(),
-        Utc::now(),
+        start_date,
+        end_date,
         1, // ID utilisateur admin (assure-toi qu'il existe en DB)
         None,
         1,
@@ -34,11 +40,14 @@ async fn test_create_recurrent_event_by_user_success() {
     let (_container, host) = get_shared_db().await;
     let pool = get_pool(host.to_string()).await;
 
+    let start_date = Utc::now();
+    let _ = sleep(Duration::from_secs(1));
+    let end_date = start_date + chrono::Duration::days(10);
     let view = CreateEventByUserQueryView::new(
         "Test Event",
         Some("Description"),
-        Utc::now(),
-        Utc::now(),
+        start_date,
+        end_date,
         1, // ID utilisateur admin (assure-toi qu'il existe en DB)
         Some(RecurrenceRule::new(ReccurenceType::Daily, Some(1), None)),
         1,
@@ -55,11 +64,14 @@ async fn test_create_event_by_group_success() {
     let (_container, host) = get_shared_db().await;
     let pool = get_pool(host.to_string()).await;
 
+    let start_date = Utc::now();
+    let _ = sleep(Duration::from_secs(1));
+    let end_date = start_date + chrono::Duration::days(10);
     let view = CreateEventByGroupQueryView::new(
         "Test Event".to_string(),
         Some("Description".to_string()),
-        Utc::now(),
-        Utc::now(),
+        start_date,
+        end_date,
         1, // ID utilisateur admin (assure-toi qu'il existe en DB)
         None,
         1,
@@ -72,15 +84,18 @@ async fn test_create_event_by_group_success() {
 }
 
 #[sqlx::test]
-async fn test_create_recurrent_event_by_groupcreate_event_by_group_queryss() {
+async fn test_create_recurrent_event_by_group() {
     let (_container, host) = get_shared_db().await;
     let pool = get_pool(host.to_string()).await;
 
+    let start_date = Utc::now();
+    let _ = sleep(Duration::from_secs(1));
+    let end_date = start_date + chrono::Duration::days(10);
     let view = CreateEventByGroupQueryView::new(
         "Test Event".to_string(),
         Some("Description".to_string()),
-        Utc::now(),
-        Utc::now(),
+        start_date,
+        end_date,
         1, // ID utilisateur admin (assure-toi qu'il existe en DB)
         Some(RecurrenceRule::new(ReccurenceType::Daily, Some(1), None)),
         1,
